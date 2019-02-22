e2ae064ef334e9b2b5a51abaf582526c8f703eac
Bryce <46943401+MundaneSoul@users.noreply.github.com>
2019-02-21 20:52:17 -0600

    Deliverable 2
    Incomplete (needs to decide user input)

---
+++ U1_L4_D2.java
@@ -0,0 +1,26 @@
+
+package u1_lesson4_deliverables;
+
+//INCOMPLETE, NEED USER INPUT ON WHAT PEPPER
+public class U1_L4_D2 {
+         public static void main(String[] args) {
+         //TODO Code
+     }
+     public enum Peppers {   
+          JALAPENO("Jalapeno", "Jal"),
+          SERRANO("Serrano", "Ser"),
+          HABANERO("Habanero", "Hab"),
+          GHOST("Ghost", "Gho"),
+          REAPER( "Reaper", "Rea");
+          
+         private String name, spice;
+        private String abbrev;
+         private Peppers (String a1, String a2){
+         this.abbrev = a1;
+         this.abbrev = a2;
+         }
+         
+         }        
+        public String getAbbreviation() {    String abbrev = null;
+return abbrev;   }}
+     
